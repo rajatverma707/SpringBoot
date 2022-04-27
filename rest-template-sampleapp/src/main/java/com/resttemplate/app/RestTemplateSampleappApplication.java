7511e59bb0d9074fa21fc32e1d309c13ec18e8bc
rajatverma <rajatverma707@gmail.com>
2022-04-27 21:03:59 +0530

    added sample app for resttemplate

---
+++ rest-template-sampleapp/src/main/java/com/resttemplate/app/RestTemplateSampleappApplication.java
@@ -0,0 +1,13 @@
+package com.resttemplate.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestTemplateSampleappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestTemplateSampleappApplication.class, args);
+	}
+
+}
---
+++ rest-template-sampleapp/src/test/java/com/resttemplate/app/RestTemplateSampleappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resttemplate.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestTemplateSampleappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
